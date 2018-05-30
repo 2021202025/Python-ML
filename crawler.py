1a9b75fbcd2d7fda1bf1bc81dafb4e95cdc6f4c0
ShadZire <gmanu21097@gmail.com>
2018-05-30 20:24:43 +0530

    Navigating

--- crawler.py
+++ crawler.py
@@ -3,9 +3,25 @@ import urllib.request
 
 source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
 soup = bs.BeautifulSoup(source, 'lxml')
+
 #print(soup.find_all('p'))
 
-print(soup.get_text())
+#print(soup.get_text())
 
-for url in soup.find_all('a'):
-    print(url.get('href'))
+#for url in soup.find_all('a'):
+    #print(url.get('href'))
+
+nav = soup.nav
+#print(nav)
+
+#for url in nav.find_all('a'):
+#    print(url.get('href'))
+
+
+##body = soup.body
+##for paragraph in body.find_all('p'):
+##    print(paragraph.text)
+
+
+for div in soup.find_all('div', class_='body'):
+    print(div.text)
