48b066b36d61cdb0ecb2212f292c3e12598b66fe
ShadZire <gmanu21097@gmail.com>
2018-05-30 18:55:03 +0530

    Messing around in soup for crawler

---
+++ crawler.py
@@ -0,0 +1,11 @@
+import bs4 as bs
+import urllib.request
+
+source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+soup = bs.BeautifulSoup(source, 'lxml')
+#print(soup.find_all('p'))
+
+print(soup.get_text())
+
+for url in soup.find_all('a'):
+    print(url.get('href'))
