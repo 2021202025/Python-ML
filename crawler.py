d64142e59a98e31fad65e1974119107c41808d25
ShadZire <gmanu21097@gmail.com>
2018-05-31 16:43:09 +0530

    Pandas and sitemaps

--- crawler.py
+++ crawler.py
@@ -1,8 +1,16 @@
 import bs4 as bs
 import urllib.request
+import pandas as pd
 
-source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
-soup = bs.BeautifulSoup(source, 'lxml')
+
+source = urllib.request.urlopen('https://pythonprogramming.net/sitemap.xml').read()
+soup = bs.BeautifulSoup(source, 'xml')
+
+for url in soup.find_all('loc'):
+    print(url.text)
+
+##source = urllib.request.urlopen('https://pythonprogramming.net/parsememcparseface/').read()
+##soup = bs.BeautifulSoup(source, 'lxml')
 
 ###print(soup.find_all('p'))
 ##
@@ -27,11 +35,16 @@ soup = bs.BeautifulSoup(source, 'lxml')
 ##    print(div.text)
 
 #table = soup.table
-table = soup.find('table')
+##table = soup.find('table')
+##
+##table_rows = table.find_all('tr')
+##
+##for tr in table_rows:
+##    td = tr.find_all('td')
+##    row = [i.text for i in td]
+##    print(row)
 
-table_rows = table.find_all('tr')
 
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text for i in td]
-    print(row)
+##dfs = pd.read_html('https://pythonprogramming.net/parsememcparseface/', header = 0)
+##for df in dfs:
+##    print(df.head())
